2fe7155d92b1fb0a842743fef41b40c24b09a5d4
Sourav Pal <spalworks@gmail.com>
2023-01-06 13:08:38 +0530

    update: variable and constant

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -7,7 +7,7 @@ public class GamblingSimulation {
 
 	public static void main(String[] args) {
 		
-		System.out.println("**************** WELCOME TO GAMBLING SIMULATOR PROBLEM ****************\n");
+		System.out.println("***************** WELCOME TO GAMBLING SIMULATOR PROBLEM ****************\n");
 	}
 
 }
